b07dd973b8bfacd8177c73627f8248c8eef7b191
Spencer Robles <spencerrobles19@gmail.com>
2023-09-24 18:56:20 +0800

    added request and return package for returnandrequest models

---
+++ demo/src/main/java/com/example/demo/model/requestModel/ProductRequest.java
@@ -0,0 +1,23 @@
+package com.example.demo.model.requestModel;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductRequest {
+
+    Long id;
+    String productName;
+    String barid;
+    String productDesc;
+    String productMedicalName;
+    int itemCount;
+    double productPrice;
+
+}
