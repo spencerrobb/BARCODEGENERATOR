c1d0a079e2d4b9ec6ef73cd7af5f04ec04e84b02
= <spencerrobles19@gmail.com>
2023-10-18 23:03:54 +0800

    disable csrf

--- demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -3,13 +3,20 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -42,6 +49,18 @@ public class SecurityConfiguration {
 //
 //        return http.build();
 //    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth->{
+//                            auth.anyRequest().authenticated();
+//                        });
+//        http.sessionManagement(session-> {
+//            session.sessionCreationPolicy(STATELESS);
+//        });
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
